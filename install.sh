4f6f51a85ee5fe7d086c46ad0379a169ac34009f
Annie Qiu <134200250+annnnie-qiu@users.noreply.github.com>
2024-09-05 22:47:49 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 pip install LLM4HW
-curl -L -O https://github.com/annnnie-qiu/download/raw/c4a9d35a35a57f743e3fe1408c6b40e0ff148c6a/provide_to_students.zip
+curl -L -O https://github.com/annnnie-qiu/download/raw/master/provide_to_students.zip
 unzip provide_to_students.zip
 rm provide_to_students.zip
